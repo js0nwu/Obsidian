76a3255a7d3d919f6292d20bdc6e740e8daa1e3b
Jason Wu <jasonwu1997@jasonwupilly.com>
2012-05-19 21:51:00 -0400

    increased timer interval

--- Obsidian/Obsidian/Form1.Designer.cs
+++ Obsidian/Obsidian/Form1.Designer.cs
@@ -125,7 +125,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 1500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // textBox5
