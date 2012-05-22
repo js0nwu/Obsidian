530e7fd4187ca25f2d0bab5954e220df4201e097
Jason Wu <jasonwu1997@jasonwupilly.com>
2012-05-22 12:27:29 -0400

    automated user file creation

--- Obsidian/Obsidian/ownerConfig.Designer.cs
+++ Obsidian/Obsidian/ownerConfig.Designer.cs
@@ -31,6 +31,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -44,7 +46,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 51);
+            this.button1.Location = new System.Drawing.Point(12, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(257, 23);
             this.button1.TabIndex = 1;
@@ -59,11 +61,29 @@
             this.textBox1.Size = new System.Drawing.Size(260, 20);
             this.textBox1.TabIndex = 2;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 20);
+            this.textBox2.TabIndex = 4;
+            // 
             // ownerConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 87);
+            this.ClientSize = new System.Drawing.Size(284, 123);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -82,5 +102,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Obsidian/Obsidian/ownerConfig.cs
+++ Obsidian/Obsidian/ownerConfig.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using ObsidianFunctions; 
 
 namespace Obsidian
 {
@@ -21,9 +22,18 @@ namespace Obsidian
         {
             
             string ownername = textBox1.Text;
+            string ownerpass = textBox2.Text; 
             StreamWriter sw = new StreamWriter("owner.bin");
             sw.Write(ownername);
             sw.Close();
+            StreamWriter sw2 = new StreamWriter("users.bin");
+            sw2.Write(ownername + ":");
+            sw2.Close();
+            StreamWriter sw3 = new StreamWriter("passwords.bin");
+            Functions ObsidFunc = new Functions();
+            string passhash = ObsidFunc.md5calc(ownerpass);
+            sw3.Write(passhash + ":");
+            sw3.Close(); 
             Form1 mainForm = new Form1();
             mainForm.setOwner();
             this.Hide();
